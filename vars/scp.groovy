4f2f0482ffe2c971f12828a27b65fc9f8c63b5e3
ankireddy1 <101354717+ankireddy1@users.noreply.github.com>
2022-04-20 12:12:37 +0530

    Update scp.groovy

--- vars/scp.groovy
+++ vars/scp.groovy
@@ -8,7 +8,7 @@ def topMaven()
 }
 def appDeploy(jobname,ip,appname)
 {
-  sh  "scp /home/ubuntu/.jenkins/workspace/${jobname}/webapp/target/webapp.war ubuntu@${ip}:/var/lib/tomcat9/webapps/${appname}"
+  sh  "scp /home/ubuntu/.jenkins/workspace/${jobname}/webapp/target/webapp.war ubuntu@${ip}:/var/lib/tomcat9/webapps/${appname}.war"
 }
 def seltest()
 {
